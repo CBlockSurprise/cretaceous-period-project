466cc010146b3763383a2707d0949ada50c217d0
CBlockSurprise <CBlockSurprise@users.noreply.github.com>
2019-12-09 20:09:21 -0800

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function setCookie(cname, cvalue, exdays) {
   var d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
   var expires = "expires="+d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";samesite:true;path=/";
 }
 
 function getCookie(cname) {
@@ -53,4 +53,8 @@ function getCookie(cname) {
     }
   }
   return "";
+}
+
+function clearCookies() {
+  document.cookie = "";
 }
