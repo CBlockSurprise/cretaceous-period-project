56c72f24c8235404421cff967bf280fc806fd8da
CBlockSurprise <CBlockSurprise@users.noreply.github.com>
2019-12-09 20:06:18 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,54 @@ var useCookies = false;
 
 function doCookies(b) {
   useCookies = b;
+  if (useCookies) {
+    setCookie("allowCookies","true",31)
+  } else {
+    setCookie("allowCookies","true",5)
+  }
 }
 
 function setup() {
-  $("#cookieAlert").modal('show');
+  let showMessage = true;
+  if (document.cookie.length > 1) {
+    let doAllowCookies = getCookie("allowCookies");
+    if (doAllowCookies != "") {
+      if (doAllowCookies == "true") {
+        showMessage = false;
+        useCookies = true;
+      } else if (doAllowCookies == "false") {
+        showMessage = false;
+      }
+    }
+  }
+  if (showMessage) {
+    $("#cookieAlert").modal('show');
+  }
 }
 
 function removeElement(elementId) {
     var element = document.getElementById(elementId);
     element.parentNode.removeChild(element);
+}
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
