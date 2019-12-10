f59d374e3bbf35ac833f91afd5fe6082e97b08cc
CBlockSurprise <CBlockSurprise@users.noreply.github.com>
2019-12-09 19:54:49 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function doCookies(b) {
   useCookies = b;
 }
 
+function setup() {
+  $("#cookieAlert").modal('show');
+}
+
 function removeElement(elementId) {
     var element = document.getElementById(elementId);
     element.parentNode.removeChild(element);
